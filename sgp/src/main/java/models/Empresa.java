f609dbe26bc1625714732e37b09bb11245f6f478
carjox <carlosbmestra@gmail.com>
2022-08-24 15:31:33 -0500

    agregacion atributos empleado y empresa

--- sgp/src/main/java/models/Empresa.java
+++ sgp/src/main/java/models/Empresa.java
@@ -1,6 +1,40 @@
 package models;
 
-public enum Empresa {
+public class Empresa {
+    private int nit;
+    private String nombre;
+    private String direccion;
+    private String telefono;
 
-    
+    public int getNit() {
+        return nit;
+    }
+
+    public void setNit(int nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
